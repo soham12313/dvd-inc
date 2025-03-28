60a73116343b40ec5e657c26c6e76685aa95c520
soham12313 <sohambensimhon124@gmail.com>
2025-03-28 22:51:25 +0300

    feat(Constants.cs): changed upgrades names

--- Assets/scripts/Constants.cs
+++ Assets/scripts/Constants.cs
@@ -45,17 +45,17 @@ public class Constants
     public const int CRITICAL_HIT_BASE_COST = 50;
 
     // Upgrades names
-    public const string SPEED_INCREASE_NAME = "speedIncrease";
-    public const string POINTS_INCREASE_NAME = "pointsIncrease";
-    public const string DVD_COUNT_NAME = "dvdCount";
-    public const string REBIRTH_SPEED_INCREASE_NAME = "rebirthSpeedIncrease";
-    public const string REBIRTH_DVD_COUNT_NAME = "rebirthDvdCount";
-    public const string REBIRTH_POINTS_INCREASE_NAME = "rebirthPointIncrease";
-    public const string CORNER_SCALE_NAME = "cornerScale";
-    public const string PERFECT_STREAK_MULTIPLIER_NAME = "perfectStreakMultiplier";
-    public const string TIMED_COMBO_MULTIPLIER_NAME = "timedComboMultiplier";
-    public const string TIMED_COMBO_DURATION_NAME = "timedComboDuration";
-    public const string CRITICAL_HIT_NAME = "criticalHit";
+    public const string SPEED_INCREASE_NAME = "Speed Boost";
+    public const string POINTS_INCREASE_NAME = "Point Surge";
+    public const string DVD_COUNT_NAME = "Multidisc";
+    public const string CORNER_SCALE_NAME = "Corner Mastery";
+    public const string PERFECT_STREAK_MULTIPLIER_NAME = "Perfect Streak";
+    public const string TIMED_COMBO_MULTIPLIER_NAME = "Timed Combo Amplifier";
+    public const string TIMED_COMBO_DURATION_NAME = "Timed Combo Extension";
+    public const string CRITICAL_HIT_NAME = "Critical Hit";
+    public const string REBIRTH_SPEED_INCREASE_NAME = "RebirthSpeedIncrease";
+    public const string REBIRTH_DVD_COUNT_NAME = "RebirthDvdCount";
+    public const string REBIRTH_POINTS_INCREASE_NAME = "RebirthPointIncrease";
 
     public static Dictionary<string, string> UPGRADES_DESCRIPTIONS = new Dictionary<string, string>
     {
